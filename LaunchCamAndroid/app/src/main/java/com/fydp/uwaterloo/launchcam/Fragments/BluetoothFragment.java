883a3b7130cfb044e912ac3d18fdfd539bb76c2a
Sahil_P <sahilcsjpereira@gmail.com>
2017-03-10 23:50:56 -0500

    more controls tab stuff

--- LaunchCamAndroid/app/src/main/java/com/fydp/uwaterloo/launchcam/Fragments/BluetoothFragment.java
+++ LaunchCamAndroid/app/src/main/java/com/fydp/uwaterloo/launchcam/Fragments/BluetoothFragment.java
@@ -1,13 +1,18 @@
 package com.fydp.uwaterloo.launchcam.Fragments;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,12 +24,29 @@ import com.fydp.uwaterloo.launchcam.Utility;
 /**
  * Created by Said Afifi on 15-Jul-16.
  */
-public class BluetoothFragment extends Fragment {
+public class BluetoothFragment extends Fragment implements View.OnClickListener{
+
+    View rootView = null;
+    private boolean isRecording = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.bluetooth_fragment, container, false);
+        rootView = inflater.inflate(R.layout.bluetooth_fragment, container, false);
+
+        // Record Button
+        FloatingActionButton recordBtn = (FloatingActionButton) rootView.findViewById(R.id.record_fab);
+        recordBtn.setOnClickListener(this);
+        // Options Button
+        Button optionsBtn = (Button) rootView.findViewById(R.id.options_btn);
+        optionsBtn.setOnClickListener(this);
+        // Bluetooth Button
+        Button bluetoothBtn = (Button) rootView.findViewById(R.id.bluetooth_btn);
+        bluetoothBtn.setOnClickListener(this);
+        // Switch Toggle
+        Button camVidSwitch = (Button) rootView.findViewById(R.id.mediaSwitch);
+        camVidSwitch.setOnClickListener(this);
+
 //        final EditText editText = (EditText) rootView.findViewById(R.id.msg_bt_et);
 //        Button sendBtn = (Button) rootView.findViewById(R.id.send_btn);
 //        sendBtn.setOnClickListener(new View.OnClickListener() {
@@ -44,5 +66,64 @@ public class BluetoothFragment extends Fragment {
 //        ImageView bluetoothBtn = (ImageView) rootView.findViewById(R.id.bluetooth_btn);
 //        redCircle.getDrawable().setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY );
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.record_fab:
+//                Utility.toast("Record", getActivity());
+                ImageView batteryIcon = (ImageView) rootView.findViewById(R.id.battery);
+                if(batteryIcon.getTag().equals(R.drawable.battery_4)){
+                    batteryIcon.setImageResource(0);
+                    batteryIcon.setImageResource(R.drawable.battery_2);
+                    batteryIcon.setTag(R.drawable.battery_2);
+                } else{
+                    batteryIcon.setImageResource(0);
+                    batteryIcon.setImageResource(R.drawable.battery_4);
+                    batteryIcon.setTag(R.drawable.battery_4);
+                }
+
+                if(!isRecording){
+                    // send command to record
+                } else{
+                    // send command to stop recording
+                }
+                isRecording = !isRecording;
+                break;
+            case R.id.options_btn:
+//                Utility.toast("Options", getActivity());
+                ImageView wifiIcon = (ImageView) rootView.findViewById(R.id.wifi);
+                if(wifiIcon.getTag().equals(R.drawable.wifi_full)){
+                    wifiIcon.setImageResource(0);
+                    wifiIcon.setImageResource(R.drawable.wifi_low);
+                    wifiIcon.setTag(R.drawable.wifi_low);
+                } else{
+                    wifiIcon.setImageResource(0);
+                    wifiIcon.setImageResource(R.drawable.wifi_full);
+                    wifiIcon.setTag(R.drawable.wifi_full);
+                }
+                break;
+            case R.id.bluetooth_btn:
+                Utility.toast("Bluetooth", getActivity());
+                break;
+            case R.id.mediaSwitch:
+//                Utility.toast("Record", getActivity());
+                FloatingActionButton recordBtn = (FloatingActionButton) rootView.findViewById(R.id.record_fab);
+                Button mediaSwitch = (Button) rootView.findViewById(R.id.mediaSwitch);
+                if(mediaSwitch.getTag().equals(R.drawable.ic_videocam_black_36dp)){
+                    mediaSwitch.setBackgroundResource(R.drawable.ic_camera_alt_black_36dp);
+                    recordBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.material_deep_teal_200)));
+                    mediaSwitch.setTag(R.drawable.ic_camera_alt_black_36dp);
+                } else{
+                    mediaSwitch.setBackgroundResource(R.drawable.ic_videocam_black_36dp);
+                    recordBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.primary_dark_material_dark)));
+                    mediaSwitch.setTag(R.drawable.ic_videocam_black_36dp);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
