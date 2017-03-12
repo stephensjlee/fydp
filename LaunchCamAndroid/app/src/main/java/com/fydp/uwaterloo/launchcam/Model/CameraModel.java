956f8403d025dc68d8d7fcef01108189983f3a74
Sahil_P <sahilcsjpereira@gmail.com>
2017-03-12 17:59:24 -0400

    Saeed changes

---
+++ LaunchCamAndroid/app/src/main/java/com/fydp/uwaterloo/launchcam/Model/CameraModel.java
@@ -0,0 +1,19 @@
+package com.fydp.uwaterloo.launchcam.Model;
+
+public class CameraModel {
+    private String login;
+    private String blog;
+    private int public_repos;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public int getPublicRepos() {
+        return public_repos;
+    }
+}
